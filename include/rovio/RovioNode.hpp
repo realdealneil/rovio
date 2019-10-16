1da8a99626d29921f10d79d53521f4b1d74566bb
Neil Johnson <realdealneil@gmail.com>
2019-10-16 17:06:53 -0600

    Fix rovio so it doesn't crash when run on subt sim...it was getting callbacks for pose and interpreting them as ground truth, which was killing the filter for some reason

--- include/rovio/RovioNode.hpp
+++ include/rovio/RovioNode.hpp
@@ -208,8 +208,8 @@ class RovioNode{
     subImu_ = nh_.subscribe("imu0", 1000, &RovioNode::imuCallback,this);
     subImg0_ = nh_.subscribe("cam0/image_raw", 1000, &RovioNode::imgCallback0,this);
     subImg1_ = nh_.subscribe("cam1/image_raw", 1000, &RovioNode::imgCallback1,this);
-    subGroundtruth_ = nh_.subscribe("pose", 1000, &RovioNode::groundtruthCallback,this);
-    subGroundtruthOdometry_ = nh_.subscribe("odometry", 1000, &RovioNode::groundtruthOdometryCallback, this);
+    subGroundtruth_ = nh_.subscribe("true_pose", 1000, &RovioNode::groundtruthCallback,this);
+    subGroundtruthOdometry_ = nh_.subscribe("true_odometry", 1000, &RovioNode::groundtruthOdometryCallback, this);
     subVelocity_ = nh_.subscribe("abss/twist", 1000, &RovioNode::velocityCallback,this);
 
     // Initialize ROS service servers.
